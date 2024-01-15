860b523f5117eb4265f3ee5de8362a772c5a2d42
peixed351 <peixed351@gmail.com>
2024-01-15 15:58:35 +0300

    set im id

--- src/settings.py
+++ src/settings.py
@@ -42,7 +42,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://github.com/kyotovision-public/multimodal-material-segmentation"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 12541256
+PREVIEW_IMAGE_ID: int = 12662389
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/multimodal-material-segmentation"
